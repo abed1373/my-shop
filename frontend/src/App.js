b90bd2ac1972704105534b000424ba9d24c54cd3
abedrivaz <abedrivaz123456@gmail.com>
2023-06-01 12:12:00 +0330

    create react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
+
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProducrScreen'
+
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <header>
-        <a href="/">myShop</a>
+        <Link to="/">myShop</Link>
       </header>
-      <main>list products  </main>
+      <Routes >
+        <Route path='/product/:slug' element={<ProductScreen/>}/>
+        <Route path='/' element={<HomeScreen/>}/>
+      </Routes>
+      <main>
+      
+      </main>
     </div>
+    </BrowserRouter>
   )
 }
 
