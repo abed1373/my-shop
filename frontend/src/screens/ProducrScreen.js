155faf6c38ccf3ed0c9c484891fba3a9ef5e9764
abedrivaz <abedrivaz123456@gmail.com>
2023-06-12 19:07:32 +0330

    Create react-Context and implement add to cart

--- frontend/src/screens/ProducrScreen.js
+++ frontend/src/screens/ProducrScreen.js
@@ -1,18 +1,19 @@
 import axios from 'axios'
-import { useEffect, useReducer } from 'react'
+import { useContext, useEffect, useReducer } from 'react'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
 import { useParams } from 'react-router-dom'
-import Button from 'react-bootstrap/Button';
-import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button'
+import Badge from 'react-bootstrap/Badge'
 
 import Rating from '../components/Rating'
 import { Helmet } from 'react-helmet-async'
 import LoadingBox from '../components/LoadingBox'
 import MessageBox from '../components/MessageBox'
 import { getError } from '../utils'
+import { Store } from '../Store'
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -44,17 +45,21 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`)
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload:  getError(err) })
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) })
       }
     }
     fetchData()
   }, [slug])
 
+  const { state, dispatch: ctxDispatch } = useContext(Store)
+  const addToCartHandler = () => {
+    ctxDispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } })
+  }
   return loading ? (
-    <LoadingBox/>
-   ) : error ? (
-     <MessageBox variant="danger">{error}</MessageBox>
-   ) : (
+    <LoadingBox />
+  ) : error ? (
+    <MessageBox variant="danger">{error}</MessageBox>
+  ) : (
     <Row>
       <Col md={6}>
         <img className="img-large" src={product.image} alt={product.name}></img>
@@ -65,52 +70,52 @@ function ProductScreen() {
             <Helmet>
               <title>{product.name}</title>
             </Helmet>
-          <h1>{product.name}</h1>
+            <h1>{product.name}</h1>
           </ListGroup.Item>
           <ListGroup.Item>
-            <Rating rating={product.rating}
-            numReviews={product.numReviews}></Rating>
-
+            <Rating
+              rating={product.rating}
+              numReviews={product.numReviews}
+            ></Rating>
           </ListGroup.Item>
-<ListGroup.Item>
-  Price: ${product.price}
-</ListGroup.Item>
-<ListGroup.Item>
-  Dscription:
-  <p>{product.description}</p>
-</ListGroup.Item>
-        
+          <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+          <ListGroup.Item>
+            Dscription:
+            <p>{product.description}</p>
+          </ListGroup.Item>
         </ListGroup>
       </Col>
       <Col md={3}>
         <Card>
           <Card.Body>
-            <ListGroup variant='flush'>
-<ListGroup.Item>
-  <Row>
-    <Col>Price:</Col>
-    <Col>${product.price}</Col>
-  </Row>
-</ListGroup.Item>
-<ListGroup.Item>
-                  <Row>
-                    <Col>Status:</Col>
-                    <Col>
-                      {product.countInStock > 0 ? (
-                        <Badge bg="success">In Stock</Badge>
-                      ) : (
-                        <Badge bg="danger">Unavailable</Badge>
-                      )}
-                    </Col>
-                  </Row>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>${product.price}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? (
+                      <Badge bg="success">In Stock</Badge>
+                    ) : (
+                      <Badge bg="danger">Unavailable</Badge>
+                    )}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              {product.countInStock > 0 && (
+                <ListGroup.Item>
+                  <div className="d-grid">
+                    <Button onClick={addToCartHandler} variant="primary">
+                      Add to Cart
+                    </Button>
+                  </div>
                 </ListGroup.Item>
-                {product.countInStock > 0 && (
-                  <ListGroup.Item>
-                    <div className="d-grid">
-                      <Button variant="primary">Add to Cart</Button>
-                    </div>
-                  </ListGroup.Item>
-                )}
+              )}
             </ListGroup>
           </Card.Body>
         </Card>
