73150366090dd2cf85fb7d154e7d1f109a7c132c
abedrivaz <abedrivaz123456@gmail.com>
2023-06-19 13:26:36 +0330

    Add-Sidebar-and-Search-Box

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,8 @@ app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
 app.use('/api/orders', orderRouter);
 
+
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message })
 })
